abd9f07f3d6c8e3e15440a32fd29515730607ee1
tisha-akter <tishaakter.jui79@gmail.com>
2023-06-02 13:19:53 +0600

    added post method for user related api

--- index.js
+++ index.js
@@ -31,14 +31,25 @@ async function run() {
     await client.connect();
 
     const menuCollection = client.db("bistroDb").collection("menu");
+    const usersCollection = client.db("bistroDb").collection("users");
     const reviewCollection = client.db("bistroDb").collection("reviews");
     const cartCollection = client.db("bistroDb").collection("carts");
 
+
+    // users related apis 
+    app.post('/users', async(req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    })
+  
+    // menu related apis 
     app.get('/menu', async(req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result);
     })
 
+    // review related api 
     app.get('/reviews', async(req, res) => {
       const result = await reviewCollection.find().toArray();
       res.send(result);
@@ -59,7 +70,7 @@ async function run() {
     
     app.post('/carts', async(req, res) => {
       const item = req.body;
-      console.log(item);
+      // console.log(item);
       const result = await cartCollection.insertOne(item);
       res.send(result);
 
