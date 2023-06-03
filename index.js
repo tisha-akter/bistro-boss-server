4ef24e3ce152c2edfc69d55c1186c725646b5365
tisha-akter <tishaakter.jui79@gmail.com>
2023-06-03 22:11:07 +0600

    added get method in users related apis

--- index.js
+++ index.js
@@ -37,6 +37,12 @@ async function run() {
 
 
     // users related apis 
+
+    app.get('/users', async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    })
+
     app.post('/users', async(req, res) => {
       const user = req.body;
       // console.log(user)
